0f8f9c3ead6029e92cee57a6b08564d0b526d6b4
goneone <kuyt1819@gmail.com>
2020-01-06 21:20:03 +0900

    vue build success - 2020.01.06

--- vue/src/components/Index.vue
+++ vue/src/components/Index.vue
@@ -1,32 +1,12 @@
 <template>
-  <!-- Material form login -->
-  <form>
-    <div class="space">
-
-    </div>
-    <p class="h4 text-center mb-4">Sign in</p>
-    <div class="grey-text">
-      <mdb-input label="Your email" icon="envelope" type="email"/>
-      <mdb-input label="Your password" icon="lock" type="password"/>
-    </div>
-    <div class="text-center">
-      <mdb-btn>Login</mdb-btn>
-    </div>
-  </form>
-  <!-- Material form login -->
+  <div>
+    <h1>this is mainpage </h1>
+  </div>
 </template>
 
 
 <script>
-  import { mdbInput, mdbBtn } from 'mdbvue';
 
-  export default {
-    name: 'Basic',
-    components: {
-      mdbInput,
-      mdbBtn
-    },
-  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ vue/vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    outputDir: "../oauth2/src/main/resources/static",
+    indexPath: "../static/index.html"
+};
